bb675e2a982e5e9acadcb7c7a3402902c138c4cc
tyerl <tylerling@proton.me>
2025-03-11 22:00:46 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'gallery-bg': '#f5f5f5',
+        'art-black': '#000000',
+        'highlight': '#d4a017',
+        'dark-bg': '#000000',
+        'dark-text': '#d4a017',
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+};
